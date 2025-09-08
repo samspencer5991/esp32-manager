5c29185bdb7e3049ef5fe50844689130c38788fd
Sam Spencer <sam.spencer5991@gmail.com>
2025-09-08 15:55:58 +1000

    Update

--- Src/esp_link.cpp
+++ Src/esp_link.cpp
@@ -6,13 +6,7 @@
 #define STATUS_GPIO_PIN		0
 #define INFO_PACKET_SIZE	64
 
-typedef enum
-{
-	LinkWaiting,	// Waiting for status line toggle from main controller
-	LinkInit,		// Status line toggle received, sending initial config data
-	LinkRunning,	// Link running normally
-	LinkError		// Error condition has occured
-} EspLinkState;
+static const char* ESP_LINK_TAG = "ESP_LINK";
 
 EspLinkState linkState;
 
@@ -21,45 +15,60 @@ void espLink_ProcessTask(void* parameter)
 	// Set the GPIO states and perform initial link setup
 	// Boot pin is used as a status toggle to preserve main chip pincount
 	pinMode(STATUS_GPIO_PIN, INPUT);
-	linkState = LinkWaiting;
+	linkState = LinkInit;
+	ESP_LOGD(ESP_LINK_TAG, "ESP Link initialising. Status: 'init'.");
 	while(1)
 	{
 		switch(linkState)
 		{
-			case LinkWaiting:
+			case LinkInit:
 				// Status toggle received
-				//if(digitalRead(STATUS_GPIO_PIN) == LOW)
-				if(1)
+				if(digitalRead(STATUS_GPIO_PIN) == LOW)
 				{
+					ESP_LOGD(ESP_LINK_TAG, "Status line LOW. Sending Device Info Packet");
 					// Build the device info data packet
 					uint8_t infoPacket[INFO_PACKET_SIZE];
 					char fwVersion[] = {FW_VERSION};
 					uint8_t fwVersionSize = sizeof(fwVersion);
 					infoPacket[0] = ESP_LINK_DEVICE_INFO_HEADER;
-					memcpy(fwVersion, &infoPacket[1], fwVersionSize);
+					memcpy(&infoPacket[1], fwVersion, fwVersionSize);
 					midi_SendSysEx(MidiSerial1, infoPacket, fwVersionSize+1, false);
+					linkState = LinkWaiting;
+					ESP_LOGD(ESP_LINK_TAG, "ESP Link status: 'waiting'.");
 				}
 				else
 				{
-					vTaskDelay(10 / portTICK_PERIOD_MS);
+					vTaskDelay(50 / portTICK_PERIOD_MS);
 				}
 			break;
 
-			case LinkInit:
+			case LinkWaiting:
+				// Wait for the status pin to be set high as confirmation from the main controller
+				while(digitalRead(STATUS_GPIO_PIN) == LOW)
+				{
+					vTaskDelay(50 / portTICK_PERIOD_MS);
+				}
+				ESP_LOGD(ESP_LINK_TAG, "Status line HIGH. Commencing run routine. ESP Link status: 'running'.");
+				linkState = LinkRunning;
 
 			break;
 
 			case LinkRunning:
+				vTaskDelay(5 / portTICK_PERIOD_MS);
 			break;
 
 			case LinkError:
 			break;
 
 		}
+		vTaskDelay(50 / portTICK_PERIOD_MS);
 	}
 }
 
-
+EspLinkState espLink_GetState()
+{
+	return linkState;
+}
 
 
 #endif // MIDI_BRIDGE
--- Src/esp_link.h
+++ Src/esp_link.h
@@ -9,17 +9,27 @@
 #define ESP_LINK_WIFI_INFO_HEADER 	0x11
 #define ESP_LINK_MIDI_DATA_HEADER 	0x12
 
-#define USBD_MIDI_ID 		0x01
-#define USBH_MIDI_ID 		0x02
-#define BLE_MIDI_ID 			0x03
-#define WIFI_RTP_MIDI_ID 	0x04
-#define SERIAL0_MIDI_ID 	0x05
-#define SERIAL1_MIDI_ID 	0x06
-#define SERIAL2_MIDI_ID 	0x07
+#define LINK_USBD_MIDI_ID 				0x00
+#define LINK_USBH_MIDI_ID 				0x01
+#define LINK_BLE_MIDI_ID 				0x02
+#define LINK_WIFI_RTP_MIDI_ID 		0x03
+#define LINK_SERIAL0_MIDI_ID 			0x04
+#define LINK_SERIAL1_MIDI_ID 			0x05
+#define LINK_SERIAL2_MIDI_ID 			0x06
 
+typedef enum
+{
+	LinkWaiting,	// Waiting for status line toggle from main controller
+	LinkInit,		// Status line toggle received, sending initial config data
+	LinkRunning,	// Link running normally
+	LinkError		// Error condition has occured
+} EspLinkState;
 
 // Free RTOS tasks
 void espLink_ProcessTask(void* parameter);
+
+
+EspLinkState espLink_GetState();
 
 #endif
 #endif // MIDI_BRIDGE_H_
